540d6fb840ad47c221b03054f8557b2d373034a6
Felipe Diaz <fel.di.rod@gmail.com>
2020-06-30 10:27:06 -0400

    Es para los primer pasos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello, world!</h1>
     </div>
   );
 }
